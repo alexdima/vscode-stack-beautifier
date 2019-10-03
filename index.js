67644df615b84ff71933aeaaf673c7627f1e99dd
Alexandru Dima <alexdima@microsoft.com>
2019-10-03 15:24:55 +0200

    safepoint

--- index.js
+++ index.js
@@ -1,67 +1,74 @@
-const COMMIT = 'ebe9ea85a0a0bb087dccc98949542605dfc51f6f';
+const COMMIT = '8eaec303ea9b12291540e729ad32b930169e67a4';
+// const SOURCEMAP_REMOTE_PATH = `/sourcemaps/${COMMIT}/core/vs/workbench/workbench.desktop.main.js.map`;
+const SOURCEMAP_REMOTE_PATH = `/sourcemaps/${COMMIT}/core/vs/workbench/services/extensions/node/extensionHostProcess.js.map`;
 
 const fs = require('fs');
+const path = require('path');
 const https = require('https');
 const cp = require('child_process');
 
-function https_get_raw_text(options) {
-    options.port = 443;
-    return new Promise((c, e) => {
-        let req = https.get(options, (resp) => {
-            let bufs = [];
-            resp.on('data', (chunk) => {
-                bufs.push(chunk);
-            });
-            resp.on('end', () => {
-                c({
-                    host: options.host,
-                    path: options.path,
-                    statusCode: resp.statusCode,
-                    headers: resp.headers,
-                    body: Buffer.concat(bufs).toString()
-                });
-            });
-        });
-        req.on("error", e);
-    });
-}
+get_sourcemap().then((fileName) => {
+	// console.log(fileName);
+	let result = cp.execSync(`node ./node_modules/stack-beautifier/stack-beautifier.js ${fileName} -t input.txt`).toString();
 
-function get_sourcemap(commit) {
-    const fileName = `${commit}.workbench.main.js.min.map`;
-    if (fs.existsSync(fileName)) {
-        return Promise.resolve(fileName);
-    }
-    return https_get_raw_text({
-        host: `ticino.blob.core.windows.net`,
-        path: `/sourcemaps/${COMMIT}/core/vs/workbench/workbench.main.js.map`
-    }).then((sourcemap) => {
-        fs.writeFileSync(fileName, sourcemap.body);
-        return fileName;
-    })
-}
+	// console.log(result);
+	const replacer = (filename, line, col) => {
+		return `[${filename}:${line}:${col}](https://github.com/Microsoft/vscode/blob/${COMMIT}/src/vs/${filename}#L${line})`;
+	}
 
-get_sourcemap(COMMIT).then((fileName) => {
-    // console.log(fileName);
-    let result = cp.execSync(`node ./node_modules/stack-beautifier/stack-beautifier.js ${fileName} -t input.txt`).toString();
+	result = result.replace(/\(([^:\[\]]+):(\d+):(\d+)\)/g, function (_, m1, m2, m3) {
+		return `(${replacer(m1, m2, m3)})`;
+	});
+	result = result.replace(/at ([^:\[\]]+):(\d+):(\d+)/g, function (_, m1, m2, m3) {
+		return `at ${replacer(m1, m2, m3)}`;
+	});
+	result = result.replace(/at ([\w_.]+)/g, function (_, m1) {
+		return `at \`${m1}\``;
+	});
 
-    // console.log(result);
-    const replacer = (filename, line, col) => {
-        return `[${filename}:${line}:${col}](https://github.com/Microsoft/vscode/blob/${COMMIT}/src/vs/${filename}#L${line})`;
-    }
+	result = result.replace(/ /g, '&nbsp;');
+	result = result.replace(/\n/g, '<br/>');
 
-    result = result.replace(/\(([^:\[\]]+):(\d+):(\d+)\)/g, function(_, m1, m2, m3) {
-        return `(${replacer(m1, m2, m3)})`;
-    });
-    result = result.replace(/at ([^:\[\]]+):(\d+):(\d+)/g, function(_, m1, m2, m3) {
-        return `at ${replacer(m1, m2, m3)}`;
-    });
-    result = result.replace(/at ([\w_.]+)/g, function(_, m1) {
-        return `at \`${m1}\``;
-    });
-
-    result = result.replace(/ /g, '&nbsp;');
-    result = result.replace(/\n/g, '<br/>');
-
-    fs.writeFileSync('output.md', `Deminified using https://github.com/alexandrudima/vscode-stack-beautifier\n\n${result}`);
-    // console.log(result);
+	fs.writeFileSync('output.md', `Deminified using https://github.com/alexandrudima/vscode-stack-beautifier\n\n${result}`);
+	// console.log(result);
 });
+
+function get_sourcemap() {
+	const fileName = get_localfilename(SOURCEMAP_REMOTE_PATH);
+	if (fs.existsSync(fileName)) {
+		return Promise.resolve(fileName);
+	}
+	return https_get_raw_text({
+		host: `ticino.blob.core.windows.net`,
+		path: SOURCEMAP_REMOTE_PATH
+	}).then((sourcemap) => {
+		fs.writeFileSync(fileName, sourcemap.body);
+		return fileName;
+	})
+}
+
+function get_localfilename(remotePath) {
+	return `${COMMIT}.${path.basename(remotePath)}`;
+}
+
+function https_get_raw_text(options) {
+	options.port = 443;
+	return new Promise((c, e) => {
+		let req = https.get(options, (resp) => {
+			let bufs = [];
+			resp.on('data', (chunk) => {
+				bufs.push(chunk);
+			});
+			resp.on('end', () => {
+				c({
+					host: options.host,
+					path: options.path,
+					statusCode: resp.statusCode,
+					headers: resp.headers,
+					body: Buffer.concat(bufs).toString()
+				});
+			});
+		});
+		req.on("error", e);
+	});
+}
